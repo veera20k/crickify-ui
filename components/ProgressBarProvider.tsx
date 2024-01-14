c59f49e2417c56203aca92322c23a4d84ea858a4
veera20k <veerakumar2k@gmail.com>
2024-01-15 00:32:54 +0530

    progressbar issue fix

--- components/ProgressBarProvider.tsx
+++ components/ProgressBarProvider.tsx
@@ -1,18 +1,20 @@
 'use client';
 
-import React from 'react'
+import React, { Suspense } from 'react'
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 function ProgressBarProvider({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
-      <ProgressBar
-        height="2px"
-        color="red"
-        options={{ showSpinner: false }}
-        shallowRouting
-      />
+      <Suspense fallback={<></>}>
+        <ProgressBar
+          height="2px"
+          color="red"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
+      </Suspense>
     </>)
 }
 
